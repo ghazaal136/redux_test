c3d10b256202508c695daa75972cc94444ea0471
ghazaal <ghazal.hajian99@gmail.com>
2024-01-15 14:26:57 +0330

    rollbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,27 @@
-import '@/assets/styles/global.scss';
-import { theme } from '@/theme/palette';
-import { ThemeProvider } from '@emotion/react';
-import type { AppProps } from 'next/app';
+import "@/assets/styles/global.scss";
+import { theme } from "@/theme/palette";
+import { ThemeProvider } from "@emotion/react";
+import type { AppProps } from "next/app";
+import React from "react";
+import { ErrorBoundary, Provider } from "@rollbar/react"; // Provider imports 'rollbar'
+import Rollbar from "rollbar";
 
 //NEXT FONT OPTIMIZATION
-import { iranSans } from '@/utils/fontsConfig';
-
+import { iranSans } from "@/utils/fontsConfig";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const rollbarConfig = {
+    accessToken:"4f2499aab47541e9b727bd9e4470a227",
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    environment: "testenv",
+  };
+
+  function TestError() {
+    const a = null;
+    return a.hello();
+  }
+
   return (
     <>
       <style
@@ -18,11 +32,14 @@ export default function App({ Component, pageProps }: AppProps) {
           --font-iranSans: ${iranSans.style.fontFamily};
         }
       `}</style>
-      <ThemeProvider theme={theme}>
-        <Component
-          {...pageProps}
-        />
-      </ThemeProvider>
+      <Provider config={rollbarConfig}>
+        {/* <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider> */}
+        <ErrorBoundary>
+          <TestError />
+        </ErrorBoundary>
+      </Provider>
     </>
   );
 }
